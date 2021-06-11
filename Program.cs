d0621bc07e2b336292db73e207d7111646e39213
Escuti <isaexecuter@gmail.com>
2021-06-11 19:24:30 +0300

    Revert "4"
    This reverts commit 37105b51e7b0b9d67f68cc5ff1165d42b3c9138b.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Another line"); Console.WriteLine("Another line");
             Console.WriteLine("Another line 2");
+            Console.WriteLine("Another line 3");
         }
     }
 }
